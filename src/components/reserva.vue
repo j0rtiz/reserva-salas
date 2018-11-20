79316bbd8955ad216ee19cf90cd0443da506713a
Jairo Ortiz <jairo.ortiz@govbr.com.br>
2018-11-20 18:08:59 -0200

    Ajustes modal.

--- src/components/reserva.vue
+++ src/components/reserva.vue
@@ -1,25 +1,28 @@
 <template>
-  <q-card class="bg-primary">
+  <q-card color="primary">
     <q-card-title class="bg-primary text-white q-py-sm">
       <q-icon class="q-pr-sm" name="event" size="23px" />
       <span class="uppercase q-subheading text-weight-bold">{{id ? 'Editar' : 'Nova'}} reserva</span>
     </q-card-title>
     <q-card-main class="row bg-white">
-      <q-field class="col-12 q-pa-sm q-pt-md" :error="$v.formulario.dtInicial.$error">
+      <q-field class="col-12 q-px-sm q-pt-sm" :error="$v.formulario.dtInicial.$error">
         <q-datetime float-label="Data inicial" type="datetime" minimal modal color="primary" format="DD/MM/YYYY - HH:mm" format24h format-model="date" :min="dtInicial" v-model="formulario.dtInicial" @blur="$v.formulario.dtInicial.$touch" />
         <div slot="helper" v-if="!$v.formulario.dtInicial.required && $v.formulario.dtInicial.$error">Campo obrigatório.</div>
       </q-field>
       <q-field class="col-12 q-pa-sm" :error="$v.formulario.dtFinal.$error">
-        <q-datetime :disable="formulario.dtInicial ? false : true" float-label="Data final" type="datetime" minimal modal color="primary" format="DD/MM/YYYY - HH:mm" format24h format-model="date" :min="dtFinal" v-model="formulario.dtFinal" @blur="$v.formulario.dtFinal.$touch" />
+        <q-datetime :disable="formulario.dtInicial ? false : true" float-label="Data final" :type="opcao ? 'datetime' : 'time'" minimal modal color="primary" format="DD/MM/YYYY - HH:mm" format24h format-model="date" :min="dtFinal" v-model="formulario.dtFinal" @blur="$v.formulario.dtFinal.$touch" />
         <div slot="helper" v-if="!$v.formulario.dtFinal.required && $v.formulario.dtFinal.$error">Campo obrigatório.</div>
       </q-field>
-      <q-field class="col-12 q-pa-sm" :error="$v.formulario.nmEvento.$error">
+      <q-field :class="formulario.dtFinal ? 'col-12 q-px-sm' : 'col-12 q-px-sm q-pt-md'">
+        <q-checkbox v-model="opcao" color="primary" label="Repetir evento" left-label style="color: #979797;" />
+      </q-field>
+      <q-field class="col-12 q-px-sm" :error="$v.formulario.nmEvento.$error">
         <q-input float-label="Evento" color="primary" clearable v-model="formulario.nmEvento" upper-case @blur="$v.formulario.nmEvento.$touch" />
         <div slot="helper" v-if="!$v.formulario.nmEvento.required && $v.formulario.nmEvento.$error">Campo obrigatório.</div>
         <div slot="helper" v-if="!$v.formulario.nmEvento.minLength && $v.formulario.nmEvento.$error">O nome do evento não pode conter menos que {{$v.formulario.nmEvento.$params.minLength.min}} caracteres.</div>
         <div slot="helper" v-if="!$v.formulario.nmEvento.maxLength && $v.formulario.nmEvento.$error">O nome do evento não pode conter mais que {{$v.formulario.nmEvento.$params.maxLength.max}} caracteres.</div>
       </q-field>
-      <q-field class="col-12 q-pa-sm">
+      <q-field :class="formulario.nmEvento ? 'col-12 q-pa-sm' : 'col-12 q-pa-sm q-pt-lg'">
         <q-input float-label="Sala" color="primary" v-model="descSala" disable />
       </q-field>
     </q-card-main>
@@ -48,8 +51,10 @@ export default {
         salaId: this.sala.id,
         usuarioId: this.$store.state.session.id
       },
+      opcao: false,
       dtInicial: Date.now(),
       dtFinal: '',
+      dtFinalMax: '',
       descSala: `${this.sala.nrSala} - ${this.sala.tiposala.tpSala}`.toUpperCase()
     }
   },
@@ -77,6 +82,7 @@ export default {
       if (!modal) {
         this.formulario.dtInicial = ''
         this.formulario.dtFinal = ''
+        this.opcao = false
         this.formulario.nmEvento = ''
         this.formulario.salaId = ''
         this.$v.formulario.$reset()
@@ -86,6 +92,7 @@ export default {
       deep: true,
       handler (dtInicial) {
         this.dtFinal = date.addToDate(dtInicial, { hours: 1 })
+        this.dtFinalMax = dtInicial ? `${date.formatDate(dtInicial, 'YYYY-MM-DD')} 23:59:59` : ''
       }
     }
   },
--- src/components/sala.vue
+++ src/components/sala.vue
@@ -1,81 +1,83 @@
 <template>
-  <q-card class="non-selectable" color="light" :style="inReserva ? bordaVermelha : inPreReserva ? bordaAmarela : bordaVerde">
-    <q-card-title class="bg-light uppercase no-padding">
-      <q-btn class="q-mr-sm" slot="right" flat round dense color="primary" icon="event" @click="$emit('sala', sala)" />
-      <q-list v-if="sala.reservas.length" no-border>
-        <q-item dense>
-          <q-item-side>
-            <q-item-tile avatar>
-              <img v-if="inReserva" src="../statics/images/Red-ball-48.png">
-              <img v-else-if="inPreReserva" src="../statics/images/Yellow-ball-48.png">
-              <img v-else src="../statics/images/Green-ball-48.png">
-            </q-item-tile>
-          </q-item-side>
-          <q-item-main>
-            <q-item-tile label>
-              <span v-if="inReserva" class="text-primary q-subheading text-weight-bold">Reservada</span>
-              <span v-else-if="inPreReserva" class="text-primary q-subheading text-weight-bold">Pré reservada</span>
-              <span v-else class="text-primary q-subheading text-weight-bold">Disponível</span>
-            </q-item-tile>
-          </q-item-main>
-        </q-item>
-      </q-list>
-      <q-list v-else no-border>
-        <q-item dense>
-          <q-item-side>
-            <q-item-tile avatar>
-              <img src="../statics/images/Green-ball-48.png">
-            </q-item-tile>
-          </q-item-side>
-          <q-item-main>
-            <q-item-tile label>
-              <span class="text-primary q-subheading text-weight-bold">Disponível</span>
-            </q-item-tile>
-          </q-item-main>
-        </q-item>
-      </q-list>
-    </q-card-title>
-    <q-card-separator class="bg-grey-4" />
-    <q-card-main class="bg-white text-primary q-pb-none">
-      <p>
-        <strong>Sala:</strong>
-        {{sala.nrSala}}
-      </p>
-      <p>
-        <strong>Tipo:</strong>
-        {{sala.tiposala.tpSala}}
-      </p>
-      <p>
-        <strong>Andar:</strong>
-        {{sala.pavimento.nmPav}}
-      </p>
-      <p>
-        <strong>Capacidade:</strong>
-        {{sala.nrCapacidade}}
-      </p>
-      <p v-if="sala.nmEquip.length" class="no-margin q-pb-sm">
-        <strong>Equipamentos:</strong>
-        <q-list dense no-border>
-          <q-item v-for="nmEquip in sala.nmEquip" :key="nmEquip.id">
-            <q-item-main :label="`&#10004; ${nmEquip}`" />
+  <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-xs-12 q-pa-sm">
+    <q-card class="non-selectable" color="light" :style="inReserva ? bordaVermelha : inPreReserva ? bordaAmarela : bordaVerde">
+      <q-card-title class="bg-light uppercase no-padding">
+        <q-btn class="q-mr-sm" slot="right" flat round dense color="primary" icon="event" @click="$emit('sala', sala)" />
+        <q-list v-if="sala.reservas.length" no-border>
+          <q-item dense>
+            <q-item-side>
+              <q-item-tile avatar>
+                <img v-if="inReserva" src="../statics/images/Red-ball-48.png">
+                <img v-else-if="inPreReserva" src="../statics/images/Yellow-ball-48.png">
+                <img v-else src="../statics/images/Green-ball-48.png">
+              </q-item-tile>
+            </q-item-side>
+            <q-item-main>
+              <q-item-tile label>
+                <span v-if="inReserva" class="text-primary q-subheading text-weight-bold">Reservada</span>
+                <span v-else-if="inPreReserva" class="text-primary q-subheading text-weight-bold">Pré reservada</span>
+                <span v-else class="text-primary q-subheading text-weight-bold">Disponível</span>
+              </q-item-tile>
+            </q-item-main>
           </q-item>
         </q-list>
-      </p>
-      <p v-else class="no-margin q-pb-xs" />
-    </q-card-main>
-    <div v-if="inReserva || inPreReserva" class="row bg-secondary">
-      <q-card-title class="col-6 bg-primary text-white text-center uppercase no-padding" style="border-radius: 0 0 0 5px">
-        <span class="q-body-1 text-weight-bold">
-          {{dtInicial | FormataDataInicial}}<span class="lowercase">h</span>
-        </span>
+        <q-list v-else no-border>
+          <q-item dense>
+            <q-item-side>
+              <q-item-tile avatar>
+                <img src="../statics/images/Green-ball-48.png">
+              </q-item-tile>
+            </q-item-side>
+            <q-item-main>
+              <q-item-tile label>
+                <span class="text-primary q-subheading text-weight-bold">Disponível</span>
+              </q-item-tile>
+            </q-item-main>
+          </q-item>
+        </q-list>
       </q-card-title>
-      <q-card-title class="col-6 bg-secondary text-white text-center uppercase no-padding" style="border-radius: 0 0 5px 0">
-        <span class="q-body-1 text-weight-bold">
-          {{dtFinal | FormataDataFinal}}<span class="lowercase">h</span>
-        </span>
-      </q-card-title>
-    </div>
-  </q-card>
+      <q-card-separator class="bg-grey-4" />
+      <q-card-main class="bg-white text-primary q-pb-none">
+        <p>
+          <strong>Sala:</strong>
+          {{sala.nrSala}}
+        </p>
+        <p>
+          <strong>Tipo:</strong>
+          {{sala.tiposala.tpSala}}
+        </p>
+        <p>
+          <strong>Andar:</strong>
+          {{sala.pavimento.nmPav}}
+        </p>
+        <p>
+          <strong>Capacidade:</strong>
+          {{sala.nrCapacidade}}
+        </p>
+        <p v-if="sala.nmEquip.length" class="no-margin q-pb-sm">
+          <strong>Equipamentos:</strong>
+          <q-list dense no-border>
+            <q-item v-for="nmEquip in sala.nmEquip" :key="nmEquip.id">
+              <q-item-main :label="`&#10004; ${nmEquip}`" />
+            </q-item>
+          </q-list>
+        </p>
+        <p v-else class="no-margin q-pb-xs" />
+      </q-card-main>
+      <div v-if="inReserva || inPreReserva" class="row bg-secondary">
+        <q-card-title class="col-6 bg-primary text-white text-center uppercase no-padding" style="border-radius: 0 0 0 5px">
+          <span class="q-body-1 text-weight-bold">
+            {{dtInicial | FormataDataInicial}}<span class="lowercase">h</span>
+          </span>
+        </q-card-title>
+        <q-card-title class="col-6 bg-secondary text-white text-center uppercase no-padding" style="border-radius: 0 0 5px 0">
+          <span class="q-body-1 text-weight-bold">
+            {{dtFinal | FormataDataFinal}}<span class="lowercase">h</span>
+          </span>
+        </q-card-title>
+      </div>
+    </q-card>
+  </div>
 </template>
 
 <script>
