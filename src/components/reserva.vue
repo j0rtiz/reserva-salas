a619ec13bb974e58524a7eac9a286f3669a73fd4
Jairo Ortiz <jairo.ortiz@govbr.com.br>
2018-11-21 13:17:57 -0200

    Ajuste form reservas.

--- src/components/reserva.vue
+++ src/components/reserva.vue
@@ -5,24 +5,13 @@
       <span class="uppercase q-subheading text-weight-bold">{{id ? 'Editar' : 'Nova'}} reserva</span>
     </q-card-title>
     <q-card-main class="row bg-white">
-      <q-field class="col-6 q-pa-sm" :error="$v.reserva.hInicial.$error">
-        <q-datetime float-label="Hora inicial" type="time" minimal modal color="primary" format="HH:mm" format24h format-model="date" v-model="reserva.hInicial" @blur="$v.reserva.hInicial.$touch" />
-        <div slot="helper" v-if="!$v.reserva.hInicial.required && $v.reserva.hInicial.$error">Campo obrigatório.</div>
+      <q-field class="col-6 q-pa-sm q-pt-md" :error="$v.formulario.dtInicial.$error">
+        <q-datetime float-label="Data inicial" type="datetime" minimal modal color="primary" format="DD/MM/YYYY - HH:mm" format24h format-model="date" :min="dtInicial" v-model="formulario.dtInicial" @blur="$v.formulario.dtInicial.$touch" />
+        <div slot="helper" v-if="!$v.formulario.dtInicial.required && $v.formulario.dtInicial.$error">Campo obrigatório.</div>
       </q-field>
-      <q-field class="col-6 q-pa-sm" :error="$v.reserva.hFinal.$error">
-        <q-datetime :disable="reserva.hInicial ? false : true" float-label="Hora final" type="time" minimal modal color="primary" format="HH:mm" format24h format-model="date" :min="horaFinal" v-model="reserva.hFinal" @blur="$v.reserva.hFinal.$touch" />
-        <div slot="helper" v-if="!$v.reserva.hFinal.required && $v.reserva.hFinal.$error">Campo obrigatório.</div>
-      </q-field>
-      <q-field :class="opcao ? 'col-6 q-pa-sm' : 'col-12 q-pa-sm'" :error="$v.reserva.dtInicial.$error">
-        <q-datetime :disable="reserva.hFinal ? false : true" :float-label="opcao ? 'Data inicial' : 'Data'" type="date" minimal modal color="primary" format="DD/MM/YYYY" format24h format-model="date" :min="dataInicial" v-model="reserva.dtInicial" @blur="$v.reserva.dtInicial.$touch" />
-        <div slot="helper" v-if="!$v.reserva.dtInicial.required && $v.reserva.dtInicial.$error">Campo obrigatório.</div>
-      </q-field>
-      <q-field v-if="opcao" class="col-6 q-pa-sm" :error="$v.reserva.dtFinal.$error">
-        <q-datetime :disable="reserva.hFinal ? false : true" float-label="Data final" type="date" minimal modal color="primary" format="DD/MM/YYYY" format24h format-model="date" :min="dataFinal" v-model="reserva.dtFinal" @blur="$v.reserva.dtFinal.$touch" />
-        <div slot="helper" v-if="!$v.reserva.dtFinal.required && $v.reserva.dtFinal.$error">Campo obrigatório.</div>
-      </q-field>
-      <q-field v-if="reserva.dtInicial" :class="reserva.hFinal ? 'col-12 q-px-sm' : 'col-12 q-px-sm q-pt-md'">
-        <q-checkbox v-model="opcao" color="primary" label="Repetir evento" left-label style="color: #979797;" />
+      <q-field class="col-6 q-pa-sm q-pt-md" :error="$v.formulario.dtFinal.$error">
+        <q-datetime :disable="formulario.dtInicial ? false : true" float-label="Data final" type="datetime" minimal modal color="primary" format="DD/MM/YYYY - HH:mm" format24h format-model="date" :min="dtFinal" v-model="formulario.dtFinal" @blur="$v.formulario.dtFinal.$touch" />
+        <div slot="helper" v-if="!$v.formulario.dtFinal.required && $v.formulario.dtFinal.$error">Campo obrigatório.</div>
       </q-field>
       <q-field class="col-12 q-pa-sm" :error="$v.formulario.nmEvento.$error">
         <q-input float-label="Evento" color="primary" clearable v-model="formulario.nmEvento" upper-case @blur="$v.formulario.nmEvento.$touch" />
@@ -30,7 +19,7 @@
         <div slot="helper" v-if="!$v.formulario.nmEvento.minLength && $v.formulario.nmEvento.$error">O nome do evento não pode conter menos que {{$v.formulario.nmEvento.$params.minLength.min}} caracteres.</div>
         <div slot="helper" v-if="!$v.formulario.nmEvento.maxLength && $v.formulario.nmEvento.$error">O nome do evento não pode conter mais que {{$v.formulario.nmEvento.$params.maxLength.max}} caracteres.</div>
       </q-field>
-      <q-field :class="formulario.nmEvento ? 'col-12 q-px-sm' : 'col-12 q-px-sm q-pt-lg'">
+      <q-field class="col-12 q-pa-sm">
         <q-input float-label="Sala" color="primary" v-model="descSala" disable />
       </q-field>
     </q-card-main>
@@ -59,17 +48,8 @@ export default {
         salaId: this.sala.id,
         usuarioId: this.$store.state.session.id
       },
-      reserva: {
-        hInicial: '',
-        hFinal: '',
-        dtInicial: '',
-        dtFinal: ''
-      },
-      opcao: false,
-      horaInicial: Date.now(),
-      horaFinal: '',
-      dataInicial: Date.now(),
-      dataFinal: '',
+      dtInicial: Date.now(),
+      dtFinal: '',
       descSala: `${this.sala.nrSala} - ${this.sala.tiposala.tpSala}`.toUpperCase()
     }
   },
@@ -79,14 +59,6 @@ export default {
         required,
         minLength: minLength(5),
         maxLength: maxLength(30)
-      }
-    },
-    reserva: {
-      hInicial: {
-        required
-      },
-      hFinal: {
-        required
       },
       dtInicial: {
         required
@@ -103,29 +75,21 @@ export default {
     },
     modal (modal) {
       if (!modal) {
-        this.opcao = false
-        this.reserva.hInicial = ''
-        this.reserva.hFinal = ''
-        this.reserva.dtInicial = ''
-        this.reserva.dtFinal = ''
+        this.formulario.dtInicial = ''
+        this.formulario.dtFinal = ''
         this.formulario.nmEvento = ''
         this.formulario.salaId = ''
         this.$v.$reset()
       }
     },
-    'reserva.hInicial' (hInicial) {
-      this.horaFinal = date.addToDate(hInicial, { hours: 1 })
-    },
-    'reserva.dtInicial' (dtInicial) {
-      this.dataFinal = date.addToDate(dtInicial, { days: 1 })
+    'formulario.dtInicial' (dtInicial) {
+      this.dtFinal = date.addToDate(dtInicial, { hours: 1 })
     }
   },
   methods: {
     Salvar (id) {
       this.$v.formulario.$touch()
-      this.$v.reserva.$touch()
-      if (!this.$v.formulario.$error && !this.$v.reserva.$error) {
-        // this.formulario.dtInicial =
+      if (!this.$v.formulario.$error) {
         let formulario = JSON.parse(JSON.stringify(this.formulario))
         let salvar = id ? this.$axios.patch(`/reservas/${id}`, formulario) : this.$axios.post('/reservas', formulario)
         salvar.then(Res => {
