7614c0ece29de86d1baa31372cf8b1987105df9d
Jairo Ortiz <jairo.ortiz@govbr.com.br>
2019-01-12 00:28:25 -0200

    Ajustes perfilmini.

--- src/components/perfilmini.vue
+++ src/components/perfilmini.vue
@@ -1,26 +1,147 @@
 <template>
-  <q-btn flat>
-    <q-popover self="top left">
-      <q-card>
-        <q-card-main>
-          <q-item>
-            <q-item-side v-if="$store.state.session.avatar">
-              <img class="avatar" round :src="$store.state.session.avatar">
+  <div class="zoom">
+    <q-btn
+      class="no-shadow perfilmini"
+      flat
+      round
+      dense
+      no-ripple
+    >
+      <q-modal
+        v-model="modal"
+        minimized
+        no-esc-dismiss
+        no-backdrop-dismiss
+      >
+        <q-card
+          class="non-selectable"
+          flat
+        >
+          <q-card-main class="text-center">
+            <div class="q-display-1 text-weight-medium">{{$NodePackage.productName}}</div>
+            <q-chip
+              class="text-weight-medium q-mb-lg"
+              color="red-4"
+              text-color="white"
+              small
+              tag
+            >Versão: {{$NodePackage.version}}</q-chip>
+            <div class="q-body-1 text-justify text-faded q-px-sm">
+              <p class="no-margin">
+                <span class="text-weight-bold ">{{$NodePackage.productName}}</span>
+                <span class="lowercase"> é um software de código aberto, para gerenciamento de reserva de salas.</span>
+              </p>
+            </div>
+          </q-card-main>
+          <q-card-actions>
+            <q-btn
+              class="full-width primary-dark"
+              color="primary"
+              label="Ok"
+              v-close-overlay
+            />
+          </q-card-actions>
+        </q-card>
+      </q-modal>
+      <img
+        v-if="$store.state.session.avatar"
+        class="avatar-header"
+        round
+        :src="$store.state.session.avatar"
+      >
+      <q-btn
+        v-else
+        class="no-shadow"
+        round
+        dense
+        no-ripple
+      >
+        <strong class="q-title text-weight-bold">{{$store.state.session.username.substring(0,1).toUpperCase()}}</strong>
+      </q-btn>
+      <q-popover
+        self="bottom right"
+        anchor="top right"
+        :offset="[0, 3]"
+      >
+        <q-list
+          class="text-primary"
+          style="width: 300px;"
+        >
+          <q-item class="q-subheading text-weight-medium text-dark q-mb-sm">
+            <q-item-side
+              v-if="$store.state.session.avatar"
+              :avatar="$store.state.session.avatar"
+            />
+            <q-item-side v-else>
+              <q-btn
+                class="primary-dark no-shadow q-mr-sm"
+                size="xl"
+                color="primary"
+                round
+                dense
+                no-ripple
+                :to="`/usuario/${$store.state.session.id}`"
+              >
+                <strong class="q-title text-weight-medium">{{$store.state.session.username.substring(0,1).toUpperCase()}}</strong>
+              </q-btn>
             </q-item-side>
-            <q-item-side v-else inverted color="primary" :letter="$store.state.session.username.substring(0,1).toUpperCase()" />
             <q-item-main>
-              <q-item-tile label v-html="$store.state.session.username" />
-              <q-item-tile sublabel v-html="$store.state.session.email" />
+              <q-item-tile label>{{$store.state.session.username}}</q-item-tile>
+              <q-item-tile
+                class="q-caption q-pt-xs"
+                sublabel
+              >{{$store.state.session.phone}}</q-item-tile>
             </q-item-main>
           </q-item>
-        </q-card-main>
-        <q-card-actions align="around">
-          <q-btn flat round dense icon="face" color="primary" :to="`/usuarios/editar/${$store.state.session.id}`" />
-          <q-btn flat round dense icon="exit_to_app" color="primary" to="/login/logout" />
-        </q-card-actions>
-      </q-card>
-    </q-popover>
-    <img v-if="$store.state.session.avatar" :src="$store.state.session.avatar" class="avatar-header" round>
-    <q-item-side v-else inverted color="secondary" :letter="$store.state.session.username.substring(0,1).toUpperCase()" />
-  </q-btn>
+          <q-item
+            class="q-body-1"
+            dense
+            :to="`/usuario/${$store.state.session.id}`"
+          >
+            <q-item-main label="Meu perfil" />
+            <q-item-side
+              right
+              icon="keyboard_arrow_right"
+              color="primary"
+            />
+          </q-item>
+          <q-item
+            class="q-body-1"
+            dense
+            link
+            @click.native="modal = true"
+          >
+            <q-item-main label="Sobre" />
+            <q-item-side
+              right
+              icon="keyboard_arrow_right"
+              color="primary"
+            />
+          </q-item>
+          <q-item
+            class="q-mt-sm"
+            dense
+          >
+            <q-item-main label="" />
+            <q-item-side
+              right
+              icon="exit_to_app"
+              color="primary"
+              @click.native="$router.push('/login/logout')"
+            />
+          </q-item>
+        </q-list>
+      </q-popover>
+    </q-btn>
+  </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      modal: false
+    }
+  }
+}
+</script>
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,26 +1,91 @@
 <template>
-  <q-page class="row bg-grey-4" :class="$q.screen.sm || $q.screen.xs ? 'justify-between items-stretch' : 'flex-center fit'">
-    <transition appear enter-active-class="animated flipInX" leave-active-class="animated flipOutX">
-      <q-card class="col-lg-4 col-md-5 col-sm-12 col-xs-12 bg-white" :class="$q.screen.width > 1600 ? 'col-xl-2' : 'col-xl-3'" :style="$q.screen.sm || $q.screen.xs ? 'border-radius: 0;' : ''">
+  <q-page
+    class="row bg-light"
+    :class="$q.screen.sm || $q.screen.xs ? 'justify-between items-stretch' : 'flex-center fit'"
+  >
+    <transition
+      appear
+      enter-active-class="animated flipInX"
+      leave-active-class="animated flipOutX"
+    >
+      <q-card
+        class="col-lg-4 col-md-5 col-sm-12 col-xs-12 bg-white"
+        :class="$q.screen.width > 1600 ? 'col-xl-2' : 'col-xl-3'"
+        :style="$q.screen.sm || $q.screen.xs ? 'border-radius: 0;' : ''"
+      >
         <q-card-title class="bg-primary text-white q-py-sm">
           <strong>{{$NodePackage.productName}}</strong>
         </q-card-title>
         <q-card-main class="row q-mt-sm">
-          <q-field class="col-xl-12 col-lg-12 col-md-12 col-sm-6 col-xs-12 q-pa-sm" label="Usuário" label-width="12" :error="$v.formulario.username.$error">
-            <q-input color="primary" v-model="formulario.username" autofocus clearable @blur="$v.formulario.username.$touch" />
-            <div slot="helper" v-if="!$v.formulario.username.required && $v.formulario.username.$error">Campo obrigatório.</div>
-            <div slot="helper" v-if="!$v.formulario.username.minLength">O nome de usuário não pode conter menos que {{$v.formulario.username.$params.minLength.min}} caracteres.</div>
-            <div slot="helper" v-if="!$v.formulario.username.maxLength">O nome de usuário não pode conter mais que {{$v.formulario.username.$params.maxLength.max}} caracteres.</div>
+          <q-field
+            class="col-xl-12 col-lg-12 col-md-12 col-sm-6 col-xs-12 q-pa-sm"
+            label="Usuário"
+            label-width="12"
+            :error="$v.formulario.username.$error"
+          >
+            <q-input
+              class="border-1"
+              inverted-light
+              color="white"
+              v-model="formulario.username"
+              autofocus
+              clearable
+              @blur="$v.formulario.username.$touch"
+            />
+            <div
+              slot="helper"
+              v-if="!$v.formulario.username.required && $v.formulario.username.$error"
+            >Campo obrigatório.</div>
+            <div
+              slot="helper"
+              v-if="!$v.formulario.username.minLength"
+            >O nome de usuário não pode conter menos que {{$v.formulario.username.$params.minLength.min}} caracteres.</div>
+            <div
+              slot="helper"
+              v-if="!$v.formulario.username.maxLength"
+            >O nome de usuário não pode conter mais que {{$v.formulario.username.$params.maxLength.max}} caracteres.</div>
           </q-field>
-          <q-field class="col-xl-12 col-lg-12 col-md-12 col-sm-6 col-xs-12 q-pa-sm" label="Senha" label-width="12" :error="$v.formulario.password.$error">
-            <q-input color="primary" v-model="formulario.password" type="password" @keyup.enter="Entrar" @blur="$v.formulario.password.$touch" />
-            <div slot="helper" v-if="!$v.formulario.password.required && $v.formulario.password.$error">Campo obrigatório.</div>
-            <div slot="helper" v-if="!$v.formulario.password.minLength">A senha não pode conter menos que {{$v.formulario.password.$params.minLength.min}} caracteres.</div>
-            <div slot="helper" v-if="!$v.formulario.password.maxLength">A senha não pode conter mais que {{$v.formulario.password.$params.maxLength.max}} caracteres.</div>
+          <q-field
+            class="col-xl-12 col-lg-12 col-md-12 col-sm-6 col-xs-12 q-pa-sm"
+            label="Senha"
+            label-width="12"
+            :error="$v.formulario.password.$error"
+          >
+            <q-input
+              class="border-1"
+              inverted-light
+              color="white"
+              v-model="formulario.password"
+              type="password"
+              @keyup.enter="Entrar"
+              @blur="$v.formulario.password.$touch"
+            />
+            <div
+              slot="helper"
+              v-if="!$v.formulario.password.required && $v.formulario.password.$error"
+            >Campo obrigatório.</div>
+            <div
+              slot="helper"
+              v-if="!$v.formulario.password.minLength"
+            >A senha não pode conter menos que {{$v.formulario.password.$params.minLength.min}} caracteres.</div>
+            <div
+              slot="helper"
+              v-if="!$v.formulario.password.maxLength"
+            >A senha não pode conter mais que {{$v.formulario.password.$params.maxLength.max}} caracteres.</div>
           </q-field>
         </q-card-main>
-        <q-card-actions class="q-ma-sm" :align="$q.screen.sm || $q.screen.xs ? 'center' : 'end'">
-          <q-btn class="full-width" color="primary" label="Entrar" icon="done" size="form" @click="Entrar" />
+        <q-card-actions
+          class="q-mx-md q-mb-md"
+          :align="$q.screen.sm || $q.screen.xs ? 'center' : 'end'"
+        >
+          <q-btn
+            class="full-width primary-dark"
+            color="primary"
+            label="Entrar"
+            icon="done"
+            size="form"
+            @click="Entrar"
+          />
         </q-card-actions>
       </q-card>
     </transition>
@@ -45,12 +110,12 @@ export default {
       username: {
         required,
         minLength: minLength(5),
-        maxLength: maxLength(25)
+        maxLength: maxLength(20)
       },
       password: {
         required,
         minLength: minLength(5),
-        maxLength: maxLength(25)
+        maxLength: maxLength(20)
       }
     }
   },
